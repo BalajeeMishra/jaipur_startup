7e2ce958ae92b240473d725a544142aed20cb583
Balajee Mishra <bala.44472@gmail.com>
2022-07-09 17:11:50 +0530

    updated

--- routes/admin/categoryofgame.js
+++ routes/admin/categoryofgame.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const router = express.Router();
-const Category = require("../admin/categoryofgame");
+const Category = require("../../model/admin/categoryofgame");
 
 router.get("/", async (req, res) => {
   const allCategory = await Category.find({});
-  return res.json(allCategory);
+  return res.status(200).json(allCategory);
 });
 
 router.post("/add", async (req, res) => {
-  const newCategory = new Category({});
+  const { nameInput, priceInput, defaultInput, orderInput } = req.body;
+  const newCategory = new Category({
+    name: nameInput,
+    pricetoenter: priceInput,
+    order: orderInput,
+    numberofPlayers: defaultInput,
+  });
   await newCategory.save();
   return res.json({ message: "New Battle created successfully" });
 });
+
+module.exports = router;
